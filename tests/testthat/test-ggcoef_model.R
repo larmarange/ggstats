e50405cb5048a3ac38d39300bdddaa7253d54f39
Joseph Larmarange <joseph@larmarange.net>
2022-10-07 18:23:02 +0200

    additional tests

--- tests/testthat/test-ggcoef_model.R
+++ tests/testthat/test-ggcoef_model.R
@@ -174,3 +174,18 @@ test_that("ggcoef_compare() does not produce an error with an include", {
   )
 
 })
+
+test_that("geom_stripped_cols() and geom_stripped_rows() works", {
+  p <- ggplot(iris) + aes(x = Species, y = Petal.Length) + geom_count()
+
+  expect_print <- function(x) {
+    expect_error(print(x), NA)
+  }
+
+  expect_print(
+    p +
+      geom_stripped_rows(odd = "blue", even = "yellow", alpha = .1, nudge_y = .5) +
+      geom_stripped_cols()
+  )
+
+})
