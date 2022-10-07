20da8bf2c0a2307b1d6562ea1b01c7c47ceb8e12
Joseph Larmarange <joseph@larmarange.net>
2022-10-07 19:35:03 +0200

    test signif_stars()

--- tests/testthat/test-ggcoef_model.R
+++ tests/testthat/test-ggcoef_model.R
@@ -190,3 +190,16 @@ test_that("geom_stripped_cols() and geom_stripped_rows() works", {
   )
 
 })
+
+
+test_that("signif_stars() works", {
+  x <- c(0.5, 0.1, 0.05, 0.01, 0.001)
+  expect_equal(
+    signif_stars(x),
+    c("", ".", "*", "**", "***")
+  )
+  expect_equal(
+    signif_stars(x, one = .15, point = NULL),
+    c("", "*", "*", "**", "***")
+  )
+})
