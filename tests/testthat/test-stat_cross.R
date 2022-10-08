673c4e704934ebf0c4e7fd0185b2f1ebef334fa4
Joseph Larmarange <joseph@larmarange.net>
2022-10-08 17:08:02 +0200

    tests for phi coefficients

--- tests/testthat/test-stat_cross.R
+++ tests/testthat/test-stat_cross.R
@@ -21,6 +21,17 @@ test_that("stat_cross()", {
     scale_size_area(max_size = 20)
   vdiffr::expect_doppelganger("stat_cross() shape-22", p)
 
+  # custom shape and fill colour based phi coefficients
+  p <- ggplot(d) +
+    aes(
+      x = Class, y = Survived, weight = Freq,
+      size = after_stat(observed), fill = after_stat(phi)
+    ) +
+    stat_cross(shape = 22) +
+    scale_fill_steps2(show.limits = TRUE) +
+    scale_size_area(max_size = 20)
+  vdiffr::expect_doppelganger("stat_cross() phi coefficients", p)
+
   # plotting the number of observations as a table
   p <- ggplot(d) +
     aes(
@@ -42,4 +53,17 @@ test_that("stat_cross()", {
     facet_grid(Sex ~ .) +
     labs(fill = "Standardized residuals")
   vdiffr::expect_doppelganger("stat_cross() residuals", p)
+})
+
+test_that("phi coefficients", {
+  res <- Titanic %>%
+    as.data.frame() %>%
+    xtabs(Freq ~ Sex + Class, data = .) %>%
+    chisq.test() %>%
+    .augment_and_add_phi() %>%
+    dplyr::mutate(.phi = round(.data$.phi, digits = 3))
+  expect_equal(
+    res$.phi,
+    c(-0.236, 0.236, -0.149, 0.149, -0.107, 0.107, 0.375, -0.375)
+  )
 })
