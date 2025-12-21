1d12747afd468b2284c5c5c223529449a071320a
Joseph Larmarange <joseph@larmarange.net>
2025-12-21 19:40:29 +0100

    update tests

--- tests/testthat/test-stat_weighted_mean.R
+++ tests/testthat/test-stat_weighted_mean.R
@@ -5,35 +5,35 @@ test_that("stat_weighted_mean()", {
   library(ggplot2)
   data(tips, package = "reshape")
 
-  vdiffr::expect_doppelganger(
+  expect_doppelganger(
     "stat_weighted_mean() point",
     ggplot(tips) +
       aes(x = day, y = total_bill) +
       geom_point()
   )
 
-  vdiffr::expect_doppelganger(
+  expect_doppelganger(
     "stat_weighted_mean() geom-default",
     ggplot(tips) +
       aes(x = day, y = total_bill) +
       stat_weighted_mean()
   )
 
-  vdiffr::expect_doppelganger(
+  expect_doppelganger(
     "stat_weighted_mean() geom-line",
     ggplot(tips) +
       aes(x = day, y = total_bill, group = 1) +
       stat_weighted_mean(geom = "line")
   )
 
-  vdiffr::expect_doppelganger(
+  expect_doppelganger(
     "stat_weighted_mean() geom-line-grouped",
     ggplot(tips) +
       aes(x = day, y = total_bill, colour = sex, group = sex) +
       stat_weighted_mean(geom = "line")
   )
 
-  vdiffr::expect_doppelganger(
+  expect_doppelganger(
     "stat_weighted_mean() geom-bar-dodge",
     ggplot(tips) +
       aes(x = day, y = total_bill, fill = sex) +
@@ -41,7 +41,7 @@ test_that("stat_weighted_mean()", {
   )
 
   # computing a proportion on the fly
-  vdiffr::expect_doppelganger(
+  expect_doppelganger(
     "stat_weighted_mean() geom-bar-dodge-percent",
     ggplot(tips) +
       aes(x = day, y = as.integer(smoker == "Yes"), fill = sex) +
@@ -51,7 +51,7 @@ test_that("stat_weighted_mean()", {
 
   # taking into account some weights
   d <- as.data.frame(Titanic)
-  vdiffr::expect_doppelganger(
+  expect_doppelganger(
     "stat_weighted_mean() titanic",
     ggplot(d) +
       aes(
